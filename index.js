1e487ec9f00e7f8051a66a5c8644c0e59acce70a
unknown <iaancaceress1@gmail.com>
2024-01-13 07:12:28 -0300

    perdon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 let si = document.getElementById("si")
 let no = document.getElementById("no")
 let body = document.getElementById("body")
-let movete =  Math.floor(Math.random() * 400);
+let movete =  Math.floor(Math.random() * 600);
 let movete1 =  Math.floor(Math.random() * 60);
 let movete2 =  Math.floor(Math.random() * 200);
 let movete3 =   Math.floor(Math.random() * 60);
@@ -13,7 +13,7 @@ si.addEventListener("click",sii)
 
  function sii(){
     body.innerHTML = `<div class="row justify-content-center m-5 ">
-    <h1 class="col-12 amo text-center">Yo tambien te amo golda</h1>
+    <h1 class="col-12 m-5 amo text-center">Yo tambien te amo golda</h1>
     <img class="col-12 col-lg-8 m-5 img"  src="./img/gato_amor.jpg" alt="">
 </div>`
 }
@@ -38,7 +38,7 @@ function noo(){
     
     
     
-    movete =  Math.floor(Math.random() * 400);
+    movete =  Math.floor(Math.random() * 600);
     movete1 =  Math.floor(Math.random() * 60);
     movete2 =  Math.floor(Math.random() * 200);
     movete3 =   Math.floor(Math.random() * 60);
