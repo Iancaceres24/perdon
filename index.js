78612b9dcb5ac777dc0c4e006bac4d9585f6eeef
unknown <iaancaceress1@gmail.com>
2024-01-13 06:55:02 -0300

    perdon

--- index.js
+++ index.js
@@ -3,18 +3,18 @@
 let si = document.getElementById("si")
 let no = document.getElementById("no")
 let body = document.getElementById("body")
-let movete =  Math.floor(Math.random() * 300);
-let movete1 =  Math.floor(Math.random() * 300);
-let movete2 =  Math.floor(Math.random() * 500);
-let movete3 =   Math.floor(Math.random() * 260);
+let movete =  Math.floor(Math.random() * 400);
+let movete1 =  Math.floor(Math.random() * 60);
+let movete2 =  Math.floor(Math.random() * 200);
+let movete3 =   Math.floor(Math.random() * 60);
 let izder = Math.floor(Math.random() * 2);
 let arabj =  Math.floor(Math.random() * 2);
 si.addEventListener("click",sii)
 
  function sii(){
     body.innerHTML = `<div class="row justify-content-center m-5 ">
-    <h2 class="col-5 amo text-center">Yo tambien te amo</h2>
-    <img class="col-8 m-5"  src="./img/gato_amor.jpg" alt="">
+    <h2 class="col-12 amo text-center">Yo tambien te amo golda</h2>
+    <img class="col-12 col-sm-8 m-5"  src="./img/gato_amor.jpg" alt="">
 </div>`
 }
 
@@ -38,10 +38,10 @@ function noo(){
     
     
     
-    movete =  Math.floor(Math.random() * 300);
-    movete1 =  Math.floor(Math.random() * 300);
-    movete2 =  Math.floor(Math.random() * 500);
-    movete3 =   Math.floor(Math.random() * 260);
+    movete =  Math.floor(Math.random() * 400);
+    movete1 =  Math.floor(Math.random() * 60);
+    movete2 =  Math.floor(Math.random() * 200);
+    movete3 =   Math.floor(Math.random() * 60);
     izder = Math.floor(Math.random() * 2);
     arabj =  Math.floor(Math.random() * 2);
 }
